4f36a02d19fa78d1d48f9d9f21bfd4716154db90
Filip <filip.gadiuta@gmail.com>
2016-01-28 20:54:28 +0200

    prefix sitewide variable in context_proc to avoid potential conflicts

--- djtransverbisblog/context_proc.py
+++ djtransverbisblog/context_proc.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 
 def base_variables(request):
-    return {'pages': Page.objects.filter(sidebar=False).order_by('page_order'),
-            'sidebar': Page.objects.filter(sidebar=True).first(),
-            'newcomms': len(Comment.objects.filter(approved_comment=False)),
+    return {'tvblog_pages': Page.objects.filter(sidebar=False).order_by('page_order'),
+            'tvblog_sidebar': Page.objects.filter(sidebar=True).first(),
+            'tvblog_newcomms': len(Comment.objects.filter(approved_comment=False)),
             'website_name': settings.WEBSITE_NAME,
             'website_slogan': settings.WEBSITE_SLOGAN}
